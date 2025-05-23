536835702732b9d5fb52d5221f0c419940f678b6
Gabfuwak <gabin.maury0@gmail.com>
2025-05-23 14:29:12 +0200

    feat: backend for function headers

--- lib/emit_c.ml
+++ lib/emit_c.ml
@@ -1,10 +1,46 @@
 open Minimir
 open Ast
 
+let ctyp_of_mirtyp typ =
+  match typ with
+    | Tstruct (_, _) -> failwith "todo: add ctyp_of_mirtyp struct"
+    | Tborrow (_, _, _) -> failwith "todo: add ctyp_of_mirtyp borrow"
+    | Tunit -> "void"
+    | Ti32 -> "int32_t"
+    | Tbool -> "bool"
+  
+
 let emit_function_header fname mir oc =
-  failwith "todo"
+  (* Ici on ignore les variables locales pour l'instant *)
+  let args = ref [] in
+  let ret_type = ref Tunit in
+
+  Hashtbl.iter(fun local this_typ -> 
+    match local with
+    | Lparam param_name -> args := (param_name, this_typ) :: !args;      
+    | Lvar _ ->  ()
+    | Lret -> ret_type := this_typ
+  ) mir.mlocals;
+
+  (*Emit ret type and fname*)
+  Printf.fprintf oc "%s %s" (ctyp_of_mirtyp !ret_type) fname;
+  
+  (*Emit args*)
+  Printf.fprintf oc "(";
+  List.iteri (
+    fun i (arg_name,arg_type) -> 
+      Printf.fprintf oc "%s %s" (ctyp_of_mirtyp arg_type) arg_name;
+      if i < (List.length !args)-1 then(
+        Printf.fprintf oc ", "
+      );
+  ) !args;
+  
+  Printf.fprintf oc ");\n"
+  
+
 
 let emit_struct_typedef struct_decl oc =
+  let _ = (struct_decl, oc) in
   failwith "todo"
 
 let emit_c prog output_file =
