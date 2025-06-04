8ef991d3e147c9c1e39a4aa7fd33548d87443aa9
Gabfuwak <gabin.maury0@gmail.com>
2025-06-04 22:48:28 +0200

    feat: emit_c basé sur les goto complet sauf rvalue et places

--- lib/emit_c.ml
+++ lib/emit_c.ml
@@ -158,10 +158,13 @@ let emit_struct_typedef struct_decl oc =
 let c_of_rvalue rval =
   failwith "todo"
 
+let c_of_place pl =
+  failwith "todo"
+
 let indent_of_scope scope = 
   String.make (scope * 4) ' '
 
-let emit_function_impl fundef mir_body oc = 
+let emit_function_impl prog fundef mir_body oc = 
   emit_function_header fundef.fname.id mir_body oc; (* On emit le header sans le ; pour s'eviter de la duplication de code*)
   let scope = ref 0 in
   Printf.fprintf oc "{\n";
@@ -184,17 +187,48 @@ let emit_function_impl fundef mir_body oc =
     (* Emit le label (note: j'aime pas faire ça comme ça, c'est de l'assembleur with extra steps, si ce commentaire reste dans le code final ma deception est immense et ma peine est inconsolable) *)
     Printf.fprintf oc "L%d:\n" curr_lbl;
     match instr with
-    | Iassign (pl_dest, rval, next_lbl) -> failwith "todo"
-    | Icall (name, args, retplace, next_lbl) -> failwith "todo"
+    | Iassign (pl_dest, rval, next_lbl) -> 
+        Printf.fprintf oc "%s%s = %s;\n" (indent_of_scope !scope) (c_of_place pl_dest) (c_of_rvalue rval);
+        Printf.fprintf oc "%sgoto L%d;\n" (indent_of_scope !scope) next_lbl
+    | Icall (name, args, retplace, next_lbl) ->
+        Printf.fprintf oc "%s" (indent_of_scope !scope);
+        let ret_type = typ_of_place prog mir_body retplace in
+        if ret_type <> Tunit then
+          Printf.fprintf oc "%s = " (c_of_place retplace) 
+        ;
+        Printf.fprintf oc "%s(" name;
+        List.iteri (
+          fun i arg_pl ->
+            if i < (List.length args) - 1 then
+              Printf.fprintf oc "%s, " (c_of_place arg_pl)
+            else
+              Printf.fprintf oc "%s" (c_of_place arg_pl)
+        ) args;
+        Printf.fprintf oc ");\n";
+
+        Printf.fprintf oc "%sgoto L%d;\n" (indent_of_scope !scope) next_lbl
+
     | Igoto (dest_lbl) -> 
-        Printf.fprintf oc "goto L%d;" dest_lbl
-    | Iif (place_to_check, then_lbl, else_lbl) -> failwith "todo"
+        Printf.fprintf oc "%sgoto L%d;\n" (indent_of_scope !scope) dest_lbl
+    | Iif (place_to_check, then_lbl, else_lbl) -> 
+        (* un peu overkill, ça pourrait se faire en une ligne mais je pense que ce sera plus simple pour faire de l'idiomatique ensuite comme ça *)
+        Printf.fprintf oc "%sif (%s) {\n" (indent_of_scope !scope) (c_of_place place_to_check);
+        scope := !scope + 1;
+        Printf.fprintf oc "%sgoto L%d;\n" (indent_of_scope !scope) then_lbl;
+        scope := !scope - 1;
+        Printf.fprintf  oc "%s} else {\n" (indent_of_scope !scope);
+        scope := !scope + 1;
+        Printf.fprintf oc "%sgoto L%d;\n" (indent_of_scope !scope) else_lbl;
+        scope := !scope - 1;
+        Printf.fprintf  oc "%s}\n" (indent_of_scope !scope);
+        
+        
     | Ireturn ->
         let ret_type = Hashtbl.find mir_body.mlocals Lret in
         if ret_type = Tunit then
-          Printf.fprintf oc "return;\n"
+          Printf.fprintf oc "%sreturn;\n" (indent_of_scope !scope)
         else
-          Printf.fprintf oc "return _ret;\n"
+          Printf.fprintf oc "%sreturn _ret;\n" (indent_of_scope !scope)
     | Ideinit _ -> () (* rien a faire *)
   ) mir_body.minstrs;
 
@@ -245,7 +279,7 @@ let emit_c prog output_file =
 
   List.iter (
     fun (fundef, body) ->
-      emit_function_impl fundef body oc
+      emit_function_impl prog fundef body oc
   ) mir_bodies;
 
   let _ = mir_bodies in (*todo, function implem*)
