27fa922dcb9caa5bae79a80b93a40fd8d7c05143
Gabfuwak <gabin.maury0@gmail.com>
2025-06-05 04:01:57 +0200

    feat: RISC-V backend stack management and control flow (waiting for assign to test CF)

--- lib/emit_riscv.ml
+++ lib/emit_riscv.ml
@@ -3,10 +3,13 @@ open Ast
 
 let indent = "  "
 
+let label_name fundef lbl = 
+  Printf.sprintf "%s_L%d" fundef.fname.id lbl
+
 let string_of_instr instr =
   Format.asprintf "%a" Print_minimir.pp_instr instr
 
-let _size_of_typ typ =
+let size_of_typ typ =
   match typ with
   | Tbool -> 1
   | Tunit -> 0
@@ -17,19 +20,53 @@ let _size_of_typ typ =
 let riscv_of_rvalue rval =
   match rval with
   | RVunit -> "" 
-  | _ -> ""
+  | _ -> failwith "todo"
 
-let riskv_of_place _pl =
-  failwith "todo"
 
+let compute_all_offsets mir_body =
+  let offset_table = Hashtbl.create 16 in
+  let current_offset = ref 0 in
+  
+  Hashtbl.iter (fun local typ ->
+    match local with
+    | Lvar id ->
+        let size = size_of_typ typ in
+        let aligned_size = ((size + 3) / 4) * 4 in
+        current_offset := !current_offset + aligned_size;
+        Hashtbl.add offset_table id (!current_offset)
+    | Lparam _ -> () (* a0-a7, TODO: ajouter le calcul correct pour generer le cas avec plus de 8 paramètres *)
+    | Lret -> ()     (* a0 *)
+  ) mir_body.mlocals;
+  
+  (!current_offset, offset_table)
+
+let riscv_of_place offset_table pl =
+  match pl with
+  | PlLocal (Lvar id) -> 
+    let offset = Hashtbl.find offset_table id in
+    Printf.sprintf "-%d(sp)" offset
+  | PlLocal (Lparam _name) -> 
+    "a0" (* TODO: mapping correct des paramètres *)
+  | PlLocal Lret -> 
+    "a0"
+  | PlDeref _ -> failwith "todo"
+  | PlField _ ->  failwith "todo" (* Valeur de retour *)
+  
 let emit_function prog fundef mir_body oc = 
+
+  let (_curr_offset, offset_table) = compute_all_offsets mir_body in
+  let frame_size = 64 in (*TODO: faire un vrai calcul*)
+
   (* emit definitions de locales Note: remplacer ça par autre chose?*)
   Printf.fprintf oc "%s:\n" fundef.fname.id;
+
+  (* allouer la stack *)
+  Printf.fprintf oc "%saddi sp, sp, -%d\n" indent frame_size;
 
   Array.iteri (
     fun curr_lbl (instr, _)->
     (*TODO possible: preprocess quels labels vont etre utilisés pour ne generer que ceux la*)
-    Printf.fprintf oc "L%d:\n" curr_lbl;
+    Printf.fprintf oc "%s:\n" (label_name fundef curr_lbl);
     Printf.fprintf oc "%s# %s\n" indent (string_of_instr instr); (*Debug/lisibilité ou on met l'instruction minimir dans l'assembleur en commentaire*)
     match instr with
     | Iassign (_, RVunit, next_lbl) -> 
@@ -37,37 +74,34 @@ let emit_function prog fundef mir_body oc =
           () (* No-op, c'est séquentiel *)
         else
           (* Avec l'implementation actuelle de minimir je crois que ça devrait etre unreachable mais autant etre robuste *)
-          Printf.fprintf oc "%sj L%d\n" indent next_lbl 
+          Printf.fprintf oc "%sj %s\n" indent (label_name fundef next_lbl) 
     | Iassign (pl_dest, rval, _) -> 
         ignore @@ failwith "todo";
-        Printf.fprintf oc "%s%s = %s;\n" indent (riskv_of_place pl_dest) (riscv_of_rvalue rval);
+        Printf.fprintf oc "%s%s = %s;\n" indent (riscv_of_place offset_table pl_dest) (riscv_of_rvalue rval);
     | Icall (name, args, retplace, _) ->
         ignore @@ failwith "todo";
         Printf.fprintf oc "%s" indent;
         let ret_type = typ_of_place prog mir_body retplace in
         if ret_type <> Tunit then
-          Printf.fprintf oc "%s = " (riskv_of_place retplace) 
+          Printf.fprintf oc "%s = " (riscv_of_place offset_table retplace) 
         ;
         Printf.fprintf oc "%s(" name;
         List.iteri (
           fun i arg_pl ->
             if i < (List.length args) - 1 then
-              Printf.fprintf oc "%s, " (riskv_of_place arg_pl)
+              Printf.fprintf oc "%s, " (riscv_of_place offset_table arg_pl)
             else
-              Printf.fprintf oc "%s" (riskv_of_place arg_pl)
+              Printf.fprintf oc "%s" (riscv_of_place offset_table arg_pl)
         ) args;
         Printf.fprintf oc ");\n";
 
     | Igoto (dest_lbl) -> 
-        Printf.fprintf oc "%sj L%d\n" indent dest_lbl
+        Printf.fprintf oc "%sj %s\n" indent (label_name fundef dest_lbl)
     | Iif (place_to_check, then_lbl, else_lbl) -> 
-        ignore @@ failwith "todo";
-        (* un peu overkill, ça pourrait se faire en une ligne mais je pense que ce sera plus simple pour faire de l'idiomatique ensuite comme ça *)
-        Printf.fprintf oc "%sif (%s) {\n" indent (riskv_of_place place_to_check);
-        Printf.fprintf oc "%sgoto L%d;\n" indent then_lbl;
-        Printf.fprintf  oc "%s} else {\n" indent;
-        Printf.fprintf oc "%sgoto L%d;\n" indent else_lbl;
-        Printf.fprintf  oc "%s}\n" indent;
+      (* Si la place n'est pas egale a 0, elle est true, et on jump au then. Sinon, on jump au else*)
+        Printf.fprintf oc "%slw t0, %s\n" indent (riscv_of_place offset_table place_to_check);
+        Printf.fprintf oc "%sbne t0, zero, %s\n" indent (label_name fundef then_lbl);
+        Printf.fprintf oc "%sj %s\n" indent (label_name fundef else_lbl);
         
         
     | Ireturn ->
@@ -77,6 +111,7 @@ let emit_function prog fundef mir_body oc =
         else
           failwith "todo"
         ;
+        Printf.fprintf oc "%saddi sp, sp, %d\n" indent frame_size; (* On rend la stack! *)
         Printf.fprintf oc "%sjr ra\n" indent 
     | Ideinit _ -> () (* rien a faire *)
   ) mir_body.minstrs;
