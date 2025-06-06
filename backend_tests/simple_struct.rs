cc932baea4424a5358361ee34dbb0358bde8d03d
Gabfuwak <gabin.maury0@gmail.com>
2025-06-06 04:33:49 +0200

    chore: add struct related tests

---
+++ backend_tests/simple_struct.rs
@@ -0,0 +1,5 @@
+struct S { a: i32, b: i32 }
+fn test() -> i32 {
+    let s : S = S { a: 10i32, b: 20i32 };
+    s.a + s.b
+}
---
+++ backend_tests/struct_arg_no_offset.rs
@@ -0,0 +1,2 @@
+struct S { a: i32, b: i32 }
+fn test(s: S) -> i32 { s.a }
